70495dd0abb163dfe80ce2aa64327ec9753f032d
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-23 21:56:19 -0700

    Add project files.

---
+++ Abstraction/Employee.cs
@@ -0,0 +1,59 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    //Base class
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+        public Employee(string firstName, string lastName, string department) 
+        { 
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+        }
+        public abstract void Gorev();
+    }
+    // Inharatance class
+    public class YazilimGelistirici : Employee
+    {
+        public YazilimGelistirici(string firstName, string lastName) : base(firstName, lastName, "Software")
+        {
+           
+        }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, Yazılım Geliştirici olarak çalışıyorum.");
+        }
+    }
+    // Inharatance class
+    public class ProjeYoneticisi : Employee
+    {
+        public ProjeYoneticisi(string firstName, string lastName)
+            : base(firstName, lastName, "Proje Yönetimi") { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, Proje Yöneticisi olarak çalışıyorum.");
+        }
+    }
+    // Inharatance class
+    public class SatisTemsilcisi : Employee
+    {
+        public SatisTemsilcisi(string firstName, string lastName)
+            : base(firstName, lastName, "Satış") { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, Satış Temsilcisi olarak çalışıyorum.");
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,11 @@
+﻿using Abstraction;
+Employee employee = new YazilimGelistirici("Hasan", "Çıldırmış");
+employee.Gorev();
+Employee employee1 = new ProjeYoneticisi("Ayşe", "Yılmaz");
+employee1.Gorev();
+Employee employee2 = new SatisTemsilcisi("Mehmet", "Demir");
+employee2.Gorev();
+
+Console.ReadKey();
+
+
